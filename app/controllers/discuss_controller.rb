7f0fa1f3625877b6b4ee6e606d98f1bfa650b255
Carlo Villaceran <cvillaceran@gmail.com>
2017-06-29 18:25:09 -0700

    finished layout for discuss board and list of subjects

---
+++ app/controllers/discuss_controller.rb
@@ -0,0 +1,3 @@
+class DiscussController < ApplicationController
+  layout "discuss"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'welcome#index', as: 'home'
 
   get '/xantham_gum', to: 'xanthan_gum#index'
+
+  get '/discuss', to: 'discuss#index'
+
+  get '/topic', to: 'topic#index'
 end
