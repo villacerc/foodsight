adb0e92b84746335ef286e3953da2c150d2ce232
Carlo Villaceran <cvillaceran@gmail.com>
2017-07-07 23:34:48 -0700

    tested site, looks OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
-<<<<<<< HEAD
+  
   has_many :replies
   has_many :subjects
 
   def full_name
     "#{first_name} #{last_name}"
   end
-=======
 
->>>>>>> origin/integration
 end
