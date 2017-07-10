9e34bcb0574804af1983959c1df76d415c49d1fc
Carlo Villaceran <cvillaceran@gmail.com>
2017-07-10 14:58:39 -0700

    ajax and react now talk

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'bcrypt'
 
 gem "faker", github: 'stympy/faker'
 
+gem "react-rails"
+
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -57,5 +59,3 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
-gem "react-rails"
--- app/assets/javascripts/components/subject.jsx
+++ app/assets/javascripts/components/subject.jsx
@@ -9,15 +9,16 @@ class Subject extends React.Component {
   render() {
     const {subject} = this.state
     return (
-      <div className="col-md-12 subject-row">
-        <span className='title'>
-          <a href={`/subjects/${subject.id}/replies`}>{subject.title}</a>
-        </span>
-        <span className='creator'>{subject.user_full_name}</span>
-        <span className='replies'>{subject.replies_count}</span>
-        <span className='views'>{Math.floor(Math.random() * 15 + 1)}</span>
-        <div className="col-md-12 line"><hr/></div>
-      </div>
+       <div className="col-md-12 subject-row">
+         <span className='title'>
+           <a href={`/subjects/${subject.id}/replies`}>{subject.title}</a>
+         </span>
+         <span className='creator'>{subject.user_full_name}</span>
+         <span className='replies'>{subject.replies_count}</span>
+         <span className='views'>{Math.floor(Math.random() * 15 + 1)}</span>
+         <div className="col-md-12 line"><hr/></div>
+     </div>
     )
+
   }
 }
